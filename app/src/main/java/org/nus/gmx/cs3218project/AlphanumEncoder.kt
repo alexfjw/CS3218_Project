365f7b41000b75343b7f1f53827d15053185455d
Alex Fong <alexfongg@gmail.com>
2018-04-18 16:56:09 +0800

    Add comments to AlphanumEncoder

--- app/src/main/java/org/nus/gmx/cs3218project/AlphanumEncoder.kt
+++ app/src/main/java/org/nus/gmx/cs3218project/AlphanumEncoder.kt
@@ -5,33 +5,41 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 
+/**
+ *
+ */
 class AlphanumEncoder() {
 
-    // 74 + 2 digits to fill
-    // ascii range + include first digit + next delimiter + end delimiter + start delimiter
-    // 122-48 +1+1+1 = 77
-    // + left bound & right bound
-    /*  start and end freq = 10060
-        next freq = 9820
-     */
+    // map a range of frequencies to ascii 0 to Z
+    // there are 77 digits to fill
+    // 122-48 +1 +3 = 78
+    // ascii_of_Z - ascii_of_0 + 1(to include 0) + 3 (START, END, NEXT)
     private val baseAscii = 48
+    // + 2 to simplify catching frequencies beyond the left & right most frequencies
     private val numFrequencies = 78 + 2
+    // frequency range: 700 - 14,000
+    // noise is usually 100-300
+    // stop at ~14,000. some speakers can't play higher than that
     private val startFrequency = 700
-    private val endFrequency = 14_000  // try to end at ~14,000. speakers can't play higher than that
-    private val spacing = 115 // used to be 120, try reducing freq a bit, endTransmission frequency is hard to detect
+    private val endFrequency = 14_000
+    // spacing between each frequency
+    private val spacing = 115
     private val frequencies = (startFrequency..endFrequency step spacing)
             .toList()
             .subList(0, numFrequencies)
 
+    // indices for special frequencies
     private val leftBoundIndex = 0
     private val rightBoundIndex = numFrequencies - 1
     private val endTransmissionIndex = numFrequencies - 2
     private val startTransmissionIndex = numFrequencies - 3
-    private val nextCharacterIndex = numFrequencies - 4
-    val startTransmissionFrequency = frequencies[startTransmissionIndex].toFloat()
-    val endTransmissionFrequency = frequencies[endTransmissionIndex].toFloat()
-    val nextCharacterFrequency = frequencies[nextCharacterIndex].toFloat()
-    val TAG = "AlphanumEncoder"
+    private val nextItemIndex = numFrequencies - 4
+    // special frequencies
+    private val startTransmissionFrequency = frequencies[startTransmissionIndex].toFloat()
+    private val endTransmissionFrequency = frequencies[endTransmissionIndex].toFloat()
+    private val nextCharacterFrequency = frequencies[nextItemIndex].toFloat()
+
+    private val TAG = "AlphanumEncoder"
 
     init {
         assert(frequencies.size == numFrequencies)
@@ -39,18 +47,25 @@ class AlphanumEncoder() {
 
     /**
      * Assumes no invalid characters
+     * Encodes input characters in the following format
+     * 1) START_FREQ * 2
+     * 2) For each character:
+     *      Character's frequency
+     *      NEXT_FREQ
+     * 3) END_FREQ * 3
+     *
+     * START_FREQ & END_FREQ are extended to reduce the chance of missing them
+     * END_FREQ is 1 longer, as it is the highest freq & is empirically easy to miss
      */
     fun stringToFrequencies(str: String): List<Float> {
         val freqs = ArrayList<Float>()
         freqs.add(startTransmissionFrequency)
         freqs.add(startTransmissionFrequency)
-        // add a NEXT, to make FreqToString code easier
         freqs.add(nextCharacterFrequency)
         str.forEach { char ->
             freqs.add(alphanumericToFrequency(char))
             freqs.add(nextCharacterFrequency)
         }
-        // do this 3x, we miss it sometimes.. very risky
         freqs.add(endTransmissionFrequency)
         freqs.add(endTransmissionFrequency)
         freqs.add(endTransmissionFrequency)
@@ -58,67 +73,63 @@ class AlphanumEncoder() {
         return freqs
     }
 
-    private val guessThreshold = 2
-    private val guessQueueSize = 3
+    // for debugging, prints the Guess type, or the character if it is a Character
+    private fun printGuesses(guesses: List<AlphanumericGuess>, textHeader: String) {
+        val classes = guesses.joinToString { guess ->
+            (guess as? Character)?.character?.toString() ?: guess.javaClass.simpleName
+        }
+        Log.i(TAG, "$textHeader: $classes")
+    }
+
     /**
-     * The contents are not expected to be sanitized
+     * Converts a list of frequencies into a String
+     * Process:
+     * 1) clip off START_FREQ & END_FREQ from the start & end of input
+     * 2) drop all guesses not NEXT or Character
+     * 3) drop all guesses that don't have an equal neighbor
+     * (transitioning between frequencies inserts an extra frequency between the original 2)
+     * (this also removes background noise. noise is 1 item long by heuristics])
+     * 4) split the guesses by NEXT
+     * 5) pick the most frequent character in each group & string them up
      */
     fun frequenciesToString(freqs: List<Float>): String {
         val rawGuesses = freqs.map { frequencyToAlphanumericGuess(it) }
-        Log.i(TAG, "Freqs\n" + freqs.joinToString())
-        val rawClasses = rawGuesses.joinToString { guess ->
-            (guess as? Character)?.character?.toString() ?: guess.javaClass.simpleName
-        }
-        Log.i(TAG, "rawClasses:  $rawClasses")
+        printGuesses(rawGuesses, "Raw Guesses")
 
-        // find the last "StartTransmission" within the first 10, and drop everything before it
+        // find the last "StartTransmission" within the first 20, and drop everything before it
         // find the first "EndTransmission" within the first 10, and drop everything before it
-        val first10 = rawGuesses.subList(0, 10)
-        var lastStart = first10.indexOfLast { it is StartTransmission }
+        val first20 = rawGuesses.subList(0, 20)
+        var lastStart = first20.indexOfLast { it is StartTransmission }
         if (lastStart == -1) lastStart = 0
         val last10 = rawGuesses.subList(rawGuesses.size - 10, rawGuesses.size)
         var lastEnd = last10.indexOfFirst { it is EndTransmission }
         if (lastEnd == -1) lastEnd = 0
-
         val clipped = rawGuesses.subList(lastStart, rawGuesses.size - 10 + lastEnd)
+        printGuesses(clipped, "Clipped Guesses")
 
-        val clippedClasses = clipped.joinToString { guess ->
-            (guess as? Character)?.character?.toString() ?: guess.javaClass.simpleName
-        }
-        Log.i(TAG, "clippedClasses:  $clippedClasses")
-
-        // drop all start & end freqs
+        // keep only Character & Next
         val filtered = clipped.filter { it is Character || it is Next }
 
         // drop all items without similar neighbors
-        val withoutNeighbors = ArrayList<AlphanumericGuess>()
+        val droppedNeighbors = ArrayList<AlphanumericGuess>()
 
         for (i in 0 until filtered.size) {
             val sameAsBefore by lazy {filtered[i] == filtered[i - 1]}
             val sameAsAfter by lazy {filtered[i] == filtered[i + 1]}
 
             if (i == 0) {
-                if (sameAsAfter) withoutNeighbors.add(filtered[i])
+                if (sameAsAfter) droppedNeighbors.add(filtered[i])
             } else if (i == filtered.size - 1) {
-                if (sameAsBefore) withoutNeighbors.add(filtered[i])
+                if (sameAsBefore) droppedNeighbors.add(filtered[i])
             } else if (sameAsAfter || sameAsBefore) {
-                withoutNeighbors.add(filtered[i])
+                droppedNeighbors.add(filtered[i])
             }
         }
+        printGuesses(droppedNeighbors, "Without Neighbors")
 
-        val neighClasses = withoutNeighbors.joinToString { guess ->
-            (guess as? Character)?.character?.toString() ?: guess.javaClass.simpleName
-        }
-        Log.i(TAG, "neighClasses:  $neighClasses")
+        val guesses = droppedNeighbors
 
-        val guesses = withoutNeighbors
-
-        val classes = guesses.joinToString { guess ->
-            (guess as? Character)?.character?.toString() ?: guess.javaClass.simpleName
-        }
-        Log.i(TAG, "classes:  $classes")
-
-        // split by next
+        // split the guesses by NEXT
         val groups = ArrayList<ArrayList<Character>>()
         var i = 0
         while (true) {
@@ -140,29 +151,26 @@ class AlphanumEncoder() {
             }
             if (i >= guesses.size) break
         }
+
+        // pick the most frequent character in each group
         val builder = StringBuilder()
         groups.forEach { builder.append(mostFrequentCharacter(it)) }
+
         return builder.toString()
     }
 
-    // if 5 of the last 8 contain NEXT,
-    // accept it as a NEXT
-    private fun detectedNext(freqs: Queue<AlphanumericGuess>): Boolean {
-        val numNexts = freqs.count { it is Next }
-        return freqs.size == guessQueueSize && numNexts >= guessThreshold
-    }
-
+    /**
+     * Returns yes, when there are at least minNumber of START_FREQs in freqs
+     */
     fun isStartTransmission(freqs: ArrayDeque<Float>, minNumber: Int): Boolean {
         val guesses = freqs.map { frequencyToAlphanumericGuess(it) }
         val (inLimit, guess) = mostFrequentType(guesses, minNumber)
-        //val classes = guesses.joinToString { guess -> guess.javaClass.simpleName }
-        //Log.i(TAG, "classes:  $classes")
-        //Log.i(TAG, "isStartTransmission")
-        //Log.i(TAG,"type: ${guess.javaClass.canonicalName}")
-        //Log.i(TAG,"detected: ${inLimit && guess is StartTransmission}")
         return inLimit && guess is StartTransmission
     }
 
+    /**
+     * Returns yes, when there are at least minNumber of END_FREQs in freqs
+     */
     fun isEndTransmission(freqs: ArrayDeque<Float>, minNumber: Int): Boolean {
         val guesses = freqs.map { frequencyToAlphanumericGuess(it) }
         val (inLimit, guess) = mostFrequentType(guesses, minNumber)
@@ -171,6 +179,7 @@ class AlphanumEncoder() {
 
     data class MostFrequentType(val inLimit: Boolean, val guess: AlphanumericGuess)
 
+    // returns the most frequent Guess & whether it has more than minNumber
     private fun mostFrequentType(guesses: List<AlphanumericGuess>, minNumber: Int): MostFrequentType {
         var startTransmission = 0
         var endTransmission = 0
@@ -199,9 +208,11 @@ class AlphanumEncoder() {
         return MostFrequentType(maxVal >= minNumber, result)
     }
 
-    // assume that frequencies are equal!
+    /**
+     * Returns the most frequent character from the list
+     */
     private fun mostFrequentCharacter(freqs: Collection<AlphanumericGuess>): Char? {
-        // might have some unknowns, and other things here
+        // get chars out of the guesses
         val sanitized = ArrayList<Char>()
         freqs.forEach { if (it is Character) sanitized.add(it.character) }
 
@@ -226,14 +237,14 @@ class AlphanumEncoder() {
         }
     }
 
-    fun frequencyToAlphanumericGuess(freq: Float): AlphanumericGuess {
+    private fun frequencyToAlphanumericGuess(freq: Float): AlphanumericGuess {
 
         val nearestIdx = findNearestFrequencyIndex(freq)
         return when (nearestIdx) {
             leftBoundIndex, rightBoundIndex -> Unknown(freq)
             startTransmissionIndex -> StartTransmission(freq)
             endTransmissionIndex -> EndTransmission(freq)
-            nextCharacterIndex -> Next(freq)
+            nextItemIndex -> Next(freq)
             else -> {
                 val charAscii = (baseAscii + nearestIdx - 1).toChar()
                 Character(freq, charAscii)
@@ -241,6 +252,10 @@ class AlphanumEncoder() {
         }
     }
 
+    /**
+     * Returns the nearest matching index in this.frequencies
+     * for the input frequency
+     */
     private fun findNearestFrequencyIndex(input: Float): Int {
         var low = 0
         var high = frequencies.size - 1
