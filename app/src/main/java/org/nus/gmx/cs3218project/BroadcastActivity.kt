766e27e9bb23565b162416466c790a97008e97d7
Alex Fong <alexfongg@gmail.com>
2018-04-18 12:18:43 +0800

    Set to 1s

--- app/src/main/java/org/nus/gmx/cs3218project/BroadcastActivity.kt
+++ app/src/main/java/org/nus/gmx/cs3218project/BroadcastActivity.kt
@@ -34,7 +34,7 @@ class BroadcastActivity : AppCompatActivity() {
         btn_play.setOnClickListener {
             val freq = et_message.text.toString()
             val freqs = AlphanumEncoder().stringToFrequencies(freq)
-            playSound(1.5f, freqs)
+            playSound(1f, freqs)
         }
 
         btn_broadcast_back.setOnClickListener{
