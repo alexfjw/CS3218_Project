f8eda1c50df2f1e8b3374e9c89b96ec8e288ef56
Guo Mingxuan <carlguo26@gmail.com>
2018-04-19 14:03:53 +0800

    Added cancel to broadcasting Running broadcasting in background thread

--- app/src/main/java/org/nus/gmx/cs3218project/BroadcastActivity.kt
+++ app/src/main/java/org/nus/gmx/cs3218project/BroadcastActivity.kt
@@ -6,15 +6,35 @@ import android.media.AudioAttributes
 import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_broadcast.*
 
 class BroadcastActivity : AppCompatActivity() {
+
+    private inner class Player : AsyncTask<Void, Void, Void>() {
+        override fun doInBackground(vararg p0: Void?): Void? {
+            val freq = et_message.text.toString()
+            val freqs = AlphanumEncoder().stringToFrequencies(freq)
+            playSound(1f, freqs)
+            return null
+        }
+
+        override fun onCancelled() {
+            super.onCancelled()
+            Log.e("onCancelled", "player task cancelled")
+        }
+    }
+
     private val RECORD_REQUEST_CODE = 101
+    private lateinit var player: Player
+    private lateinit var mAudioTrack: AudioTrack
+    private var isPlaying: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +52,21 @@ class BroadcastActivity : AppCompatActivity() {
 
     private fun init() {
         btn_play.setOnClickListener {
-            val freq = et_message.text.toString()
-            val freqs = AlphanumEncoder().stringToFrequencies(freq)
-            playSound(1f, freqs)
+            if (!isPlaying) {
+                player = Player()
+                player.execute()
+                isPlaying = true
+            } else {
+                Toast.makeText(this, "Message is already being played!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btn_broadcast_cancel.setOnClickListener {
+            if (::mAudioTrack.isInitialized) {
+                stopAudioTrack()
+            } else {
+                Log.e("Cancel", "mAudioTrack is not initialized")
+            }
         }
 
         btn_broadcast_back.setOnClickListener{
@@ -42,11 +74,18 @@ class BroadcastActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        if (::mAudioTrack.isInitialized) {
+            stopAudioTrack()
+        }
+    }
+
     private fun playSound(duration: Float, freqs: List<Float>) {
         val dur = duration * 44100
         val mBufferSize = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_OUT_MONO,  AudioFormat.ENCODING_PCM_8BIT)
 //        var mAudioTrack = AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, mBufferSize, AudioTrack.MODE_STREAM)
-        val mAudioTrack = AudioTrack(AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build(),
+        mAudioTrack = AudioTrack(AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build(),
                 AudioFormat.Builder().setEncoding(AudioFormat.ENCODING_PCM_16BIT).setSampleRate(44100).setChannelMask(AudioFormat.CHANNEL_OUT_MONO).build(),
                 mBufferSize, AudioTrack.MODE_STREAM, AudioManager.AUDIO_SESSION_ID_GENERATE)
         val mSound = DoubleArray(44100)
@@ -63,8 +102,16 @@ class BroadcastActivity : AppCompatActivity() {
             Log.e("Current freq: ", freqs[j].toString())
             mAudioTrack.write(mBuffer, 0, mSound.size)
         }
-        mAudioTrack.stop()
-        mAudioTrack.release()
+        stopAudioTrack()
+    }
 
+    private fun stopAudioTrack() {
+        try {
+            mAudioTrack.stop()
+            mAudioTrack.release()
+        } catch (e: IllegalStateException) {
+            Log.e("mAudioTrack", "mAudioTrack cancelled")
+        }
+        isPlaying = false
     }
 }
